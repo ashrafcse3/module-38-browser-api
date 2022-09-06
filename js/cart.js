52d8d8c4d997be8676daf419b08566071a89aa47
Ashraful Alam <ashrafcse3@gmail.com>
2022-09-06 19:38:04 +0100

    product displayed with a ul

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,22 @@
 const addNewProduct = () => {
     const productName = getInputFieldById('name-field');
     const productQuantity = getInputFieldById('quantity-field');
-    console.log(productName, productQuantity);
+    displayProducts(productName, productQuantity);
+    addDetailsToLS(productName, productQuantity);
 };
 
-const cart = {};
+const cart = [];
 
 const getInputFieldById = fieldID => {
     const fieldName = document.getElementById(fieldID);
     const fieldValue = fieldName.value;
+    fieldName.value = '';
     return fieldValue;
+};
+
+const displayProducts = (product, quantity) => {
+    const ulContainer = document.getElementById('ul-container');
+    const li = document.createElement('li');
+    li.textContent = `${product}: ${quantity}`;
+    ulContainer.appendChild(li);
 };
